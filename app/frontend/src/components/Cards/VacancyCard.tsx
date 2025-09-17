0dea1037b0b60ca83bf8a52a4230ff84b8975cee
Konstantin Shagan <konstantinshagan@gmail.com>
2025-09-17 14:52:41 +0300

    feat: added vacancyCard, utils/FormatSalary

---
+++ app/frontend/src/components/Cards/VacancyCard.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { formatSalary } from "../utils/FormatSalary";
+import { Link } from '@inertiajs/react';
+
+interface VacancyProps {
+  title: string;
+  description: string;
+  company: string;
+  salary: string;
+  city: string;
+  id: number;
+  key_skills: string[];
+}
+
+export const VacancyCard: React.FC<VacancyProps> = (props) => {
+  const { title, description, company, salary, city, id, key_skills } = props;
+  
+  const truncatedDescription = (description: string) => description.length > 50 ? `${description.substring(0, 50)}...` : description;
+
+  return (
+    <Link href={`/vacancies/${id}`}>
+        <div className="w-300 mx-auto border border-gray-200 rounded-lg shadow-sm p-6 m-4">
+      {/* Информация о должности и город */}
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold text-[#0d2e4e] mb-2">{title}</h1>
+        <p className="text-xl text-[#69717d]">{company} &bull; {city}</p>
+      </div>
+      {/* Зарплата и описание*/}   
+      <div className="flex justify-between items-start mb-4">
+        <div>
+          <p className="text-xl text-[#69717d] mb-3">Зарплата:</p>
+          <p className="text-xl text-[#69717d]">{truncatedDescription(description)}</p>
+        </div>
+        <div className="inline-flex items-center px-4 py-0.5 rounded-full bg-[#20B0B4]">
+          <span className="text-white font-semibold">{formatSalary(salary)}</span>
+        </div>
+      </div>
+      {/* Ключевые навыки */}
+      <div className="flex gap-3">
+        {key_skills.map((skill) => (
+          <div className="inline-flex items-center px-4 py-0.5 border border-gray-200 rounded-full">
+            <span className="font-bold text-[#0d2e4e]">{skill}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+    </Link>
+
+  )
+}
