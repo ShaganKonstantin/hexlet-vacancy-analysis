4014c65a35742731ed969f17d775b08da34f531f
Konstantin Shagan <konstantinshagan@gmail.com>
2025-09-24 12:00:18 +0300

    deleted test vacancy in HomePage

--- app/frontend/src/components/pages/HomePage.tsx
+++ app/frontend/src/components/pages/HomePage.tsx
@@ -1,19 +1,9 @@
-import { VacancyCard } from "../vacancy";
 
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Главная</h1>
       <p className="mt-4">Информация.</p>
-      <VacancyCard 
-      title="Frontend Developer"
-      description="Создавать интерфейсы и компоненты"
-      company="Tech Corp"
-      salary="95000 - 125000 руб"
-      city="Москва"
-      id={1}
-      key_skills={["React", "TypeScript", "CSS"]}
-      />
     </div>
   );
 };
